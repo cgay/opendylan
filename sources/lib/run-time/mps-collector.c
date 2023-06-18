ec0f9507ae74105c2330f8d861cb6af6fa7fc1ef
Peter S. Housel <housel@acm.org>
2023-06-17 18:47:50 -0700

    run-time: Correct MVFF pool creation usage
    * sources/lib/run-time/collector.c (MISCMAXSIZE): Remove.
    
    * sources/lib/run-time/mps-collector.c
      (dylan_init_memory_manager): Pass arguments required by the
       MVFF pool class to mps_pool_create.

--- sources/lib/run-time/mps-collector.c
+++ sources/lib/run-time/mps-collector.c
@@ -1576,7 +1576,9 @@ MMError dylan_init_memory_manager(void)
   /* Create the MVFF pool for miscellaneous objects. */
   /* This is also used for wrappers. */
   res = mps_pool_create(&misc_pool, arena, mps_class_mvff(),
-                        MISCEXTENDBY, MISCAVGSIZE, MISCMAXSIZE);
+                        MISCEXTENDBY, MISCAVGSIZE,
+                        (size_t) 16,
+                        TRUE, TRUE, TRUE);
   if (res) { init_error("create misc pool"); return(res); }
 
   wrapper_pool = misc_pool;
