31a6028953e0beb94980391b6f02966a366859b2
Carl Gay <carlgay@gmail.com>
2025-06-30 21:41:14 -0400

    system: Use getpwuid_r for file-property(file, #"author")

--- sources/system/unix-portability.c
+++ sources/system/unix-portability.c
@@ -147,8 +147,8 @@ int system_concurrent_thread_count(void)
   return (int) count;
 }
 
-// Store the homedir associated with `username` into `homedir`.  Return 0 on success,
-// -1 on failure.
+// Store the homedir associated with `username` into `homedir`. `homedir_size` is the
+// size of the `homedir` buffer. Returns 0 on success, -1 on failure.
 int system_user_homedir (const char* username, char* homedir, int homedir_size) {
   int passwd_bufsize = 0;
   if ((passwd_bufsize = sysconf(_SC_GETPW_R_SIZE_MAX)) == -1) {
@@ -165,5 +165,26 @@ int system_user_homedir (const char* username, char* homedir, int homedir_size) 
     return -1;
   }
   strncpy(homedir, pwd.pw_dir, len);
+  return 0;
+}
+
+// Store the username associated with `uid` into `username`. `username_size` is the size
+// of the `username` buffer.  Returns 0 on success, -1 on failure.
+int system_passwd_username_from_uid (uid_t uid, char* username, int username_size) {
+  int passwd_bufsize = 0;
+  if ((passwd_bufsize = sysconf(_SC_GETPW_R_SIZE_MAX)) == -1) {
+    return -1;
+  }
+  char buffer[passwd_bufsize];
+  struct passwd pwd;
+  struct passwd *result = NULL;
+  if (getpwuid_r(uid, &pwd, buffer, passwd_bufsize, &result) != 0 || !result) {
+    return -1;
+  }
+  int len = strlen(pwd.pw_name);
+  if (len >= username_size) {
+    return -1;
+  }
+  strncpy(username, pwd.pw_name, len);
   return 0;
 }
